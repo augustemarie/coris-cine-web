6bcd8e4c25ce7e5e694d93498be8f62365bfac8e
ludndev <ludndev@gmail.com>
2022-08-18 11:31:38 +0100

    fix mix content for slick

--- __templates/public/partials/header.php
+++ __templates/public/partials/header.php
@@ -8,7 +8,7 @@
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-    <link rel="stylesheet" type="text/css" href="http://kenwheeler.github.io/slick/slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="//kenwheeler.github.io/slick/slick/slick-theme.css"/>
 
     <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
